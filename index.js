70815c7e5222d7f5f69368f3e90607d374e95bc1
Ivan Tryhuba <74875731+ivantryhuba@users.noreply.github.com>
2021-05-17 23:49:28 +0300

    Финиш

---
+++ index.js
@@ -0,0 +1,52 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
+refs.startBtn.addEventListener('click', startRandom);
+refs.stopBtn.addEventListener('click', stopRandom);
+
+let randomIntervalId = null;
+
+function setRandomColor() {
+  const randomBgColor = colors[randomIntegerFromInterval(0, colors.length)];
+  refs.bodyEl.style.backgroundColor = `${randomBgColor}`;
+  console.log(`${randomBgColor}`);
+}
+
+function startRandom() {
+  refs.startBtn.removeEventListener('click', startRandom);
+  console.log('Старт');
+
+  refs.startBtn.style.backgroundColor = '#00ff7f';
+  refs.stopBtn.style.backgroundColor = '#ff0000';
+
+  randomIntervalId = setInterval(() => {
+    console.log('Меняем цвет раз в секунду');
+    setRandomColor();
+  }, 1000);
+}
+
+function stopRandom() {
+  refs.startBtn.addEventListener('click', startRandom);
+  console.log('Стоп');
+
+  refs.startBtn.style.backgroundColor = '#06e706';
+  refs.stopBtn.style.backgroundColor = '#B22222';
+
+  clearInterval(randomIntervalId);
+}
+
+function randomIntegerFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
